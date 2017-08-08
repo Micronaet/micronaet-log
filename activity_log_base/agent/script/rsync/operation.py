02df6a5a353ad95aab7f3e06bda54539c5abb347
nicothebrush <nicola.riolini@gmail.com>
2017-08-08 11:34:47 +0200

    fast git

--- activity_log_base/agent/script/rsync/operation.py
+++ activity_log_base/agent/script/rsync/operation.py
@@ -64,7 +64,7 @@ config.read([fullname])
 # Folder:
 path = config.get('operation', 'path') 
 folders = eval(config.get('operation', 'folders'))
-exclude = eval(congif.get('operation', 'exclude'))
+exclude = eval(config.get('operation', 'exclude'))
 history = eval(config.get('operation', 'history'))
 from_folder = os.path.join(path, 'mount')
 to_folder = os.path.join(path, '0')
