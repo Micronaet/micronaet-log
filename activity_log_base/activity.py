80eacfbbfafde7f86e234fe44352020f0710402c
nicothebrush <nicola.riolini@gmail.com>
2017-09-01 16:40:57 +0200

    fast git

--- activity_log_base/activity.py
+++ activity_log_base/activity.py
@@ -271,7 +271,7 @@ class LogActivity(orm.Model):
                          color: white;
                      }
                 </style>
-                <table>
+                <table class='table_bf'>
                     <tr>
                         <th>Su</th><th>Mo</th><th>Tu</th><th>We</th><th>Th</th>
                         <th>Fr</th><th>Sa</th>
