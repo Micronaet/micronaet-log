6878be6aab222dd05383cfc744d93a1099ed37bd
nicothebrush <nicola.riolini@gmail.com>
2018-06-29 17:51:05 +0200

    add management agent for temperature

---
+++ activity_log_base/agent/script/temperature/operation.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import sys
+import pickle
+import ConfigParser
+from datetime import datetime
+import hpilo
+#import telepot
+
+# -----------------------------------------------------------------------------
+#                                UTILITY:
+# -----------------------------------------------------------------------------
+def closing_operations(log_f):
+    ''' Operation that will be done at the end of the script
+    '''
+    for mode in log_f:
+        log_f[mode].close()
+    sys.exit()
+
+# -----------------------------------------------------------------------------
+#                                Parameters
+# -----------------------------------------------------------------------------
+# Extract config file name from current name
+path, name = os.path.split(os.path.abspath(__file__))
+fullname = os.path.join(path, 'operation.cfg')
+
+# Log folder:
+log_folder = os.path.join(path, 'log') # log folder path
+os.system('mkdir -p %s' % log_folder)
+
+config = ConfigParser.ConfigParser()
+config.read([fullname])
+
+# Read from config file:
+origin = config.get('operation', 'origin') # XXX not used
+error = config.get('operation', 'error')
+
+# Ilo Parameter:
+ilo_hostname = config.get('ilo', 'hostname')
+ilo_port = eval(config.get('ilo', 'port'))
+ilo_username = config.get('ilo', 'username')
+ilo_password = config.get('ilo', 'password')
+ilo_timeout = eval(config.get('ilo', 'timeout'))
+
+# Alarm temperature:
+warning = eval(config.get('temperature', 'warning'))
+error = eval(config.get('temperature', 'error'))
+
+# Telegram setup:
+#telegram_token = config.get('telegram', 'token')
+#telegram_group = config.get('telegram', 'group')
+
+# -----------------------------------------------------------------------------
+# Log event:
+# -----------------------------------------------------------------------------
+# 1. Clean log file and create new:
+log = {
+    'info': os.path.join(log_folder, 'info.log'),
+    'warning': os.path.join(log_folder, 'warning.log'),
+    'error': os.path.join(log_folder, 'error.log'),
+    }
+
+# Remove log file and create new:
+log_f = {}
+for mode in log:
+    try:
+        log_f[mode] = open(log[mode], 'w')
+        print '[INFO] File log reset: %s' % log[mode]
+    except:
+        print '[WARNING] File log not found: %s' % log[mode]
+
+# -----------------------------------------------------------------------------
+#                                Access Ilo
+# -----------------------------------------------------------------------------
+ilo = hpilo.Ilo(
+    hostname=hostname, 
+    login=username, 
+    password=password,
+    timeout=timeout,
+    port=port,
+    #protocol=None,
+    #delayed=False, 
+    #ssl_version=None
+    )
+
+# Get temperature status
+temperature = ilo.get_embedded_health()['temperature']
+status = temperature['01-Inlet Ambient']['status']
+degree = temperature['01-Inlet Ambient']['currentreading'][0]
+
+# Check temperature range for error:
+if degree >= error: 
+    log_f['error'].write('Rilevato %s°C, passato il range massimo: %s°C' % (
+            degree, error))
+elif degree >= warning: 
+    log_f['warnint'].write('Rilevato %s°C, passato range di allerta: %s°C' % (
+            degree, warning))
+else:
+    log_f['info'].write('Rilevato %s°C, nel range corretto < %s°C' % (
+        degree, warning))
+
+# -----------------------------------------------------------------------------
+#                              Comunicate Telegram
+# -----------------------------------------------------------------------------
+# Notifcation if info time out or error:
+#bot = telepot.Bot(telegram_token)
+#bot.getMe()
+#bot.sendMessage(telegram_group, status_text)
+    
+# CLosing operations:
+closing_operations(log_f)
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- activity_log_base/agent/script/temperature/readme.md
+++ activity_log_base/agent/script/temperature/readme.md
@@ -3,3 +3,13 @@ Temperature check
 ====================
 
 Manage temperature check with iLo interfaced and raise warning
+
+======================
+Dependency to install:
+======================
+
+# HP ilo:
+pip install python-hpilo
+
+# Telegram:
+pip install telepot
