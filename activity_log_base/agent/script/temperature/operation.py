d7e98e484fecb45b61bb04316c314387cd665b2f
nicothebrush <nicola.riolini@gmail.com>
2018-06-29 18:03:59 +0200

    generic bugfix (fast message)

--- activity_log_base/agent/script/temperature/operation.py
+++ activity_log_base/agent/script/temperature/operation.py
@@ -108,13 +108,13 @@ degree = temperature['01-Inlet Ambient']['currentreading'][0]
 
 # Check temperature range for error:
 if degree >= error: 
-    log_f['error'].write('\n<br/>Rilevato %s°C, passato il range massimo: %s°C' % (
+    log_f['error'].write('<br/>Rilevato %s°C, passato il range massimo: %s°C' % (
             degree, error))
 elif degree >= warning: 
-    log_f['warnint'].write('\n<br/>Rilevato %s°C, passato range di allerta: %s°C' % (
+    log_f['warnint'].write('<br/>Rilevato %s°C, passato range di allerta: %s°C' % (
             degree, warning))
 else:
-    log_f['info'].write('\n<br/>Rilevato %s°C, nel range corretto < %s°C' % (
+    log_f['info'].write('<br/>Rilevato %s°C, nel range corretto < %s°C' % (
         degree, warning))
 
 # -----------------------------------------------------------------------------
