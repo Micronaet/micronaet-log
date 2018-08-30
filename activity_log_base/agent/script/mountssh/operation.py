8e9d6a38cdf578ec83b0a354dbd961494c0e20e5
nicothebrush <nicola.riolini@gmail.com>
2018-08-30 09:34:08 +0200

    generic bugfix (fast message)

--- activity_log_base/agent/script/mountssh/operation.py
+++ activity_log_base/agent/script/mountssh/operation.py
@@ -83,6 +83,7 @@ for mode in log:
 # -----------------------------------------------------------------------------
 # Check mount procedure:
 # -----------------------------------------------------------------------------
+import pdb; pdb.set_trace()
 res = os.popen(check_command).read()
 if res.startswith(esit):
     log_f['info'].write('Correttamente montato\n')
