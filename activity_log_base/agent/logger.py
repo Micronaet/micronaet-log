d50e2f7c4770bdfd0836e47460d4ed0f2ed13e96
nicothebrush <nicola.riolini@gmail.com>
2022-05-10 10:52:55 +0200

    generic bugfix (fast message)

--- activity_log_base/agent/logger.py
+++ activity_log_base/agent/logger.py
@@ -215,7 +215,7 @@ if code_activity.upper() == 'PICKLE':
     # -------------------------------------------------------------------------
     # Try to re-update:
     remove_item = []
-    update_db = erp_error.get('update', {}).iteritems()
+    update_db = erp_error.get('update', {})
     for update_id in update_db:
         data = update_db[update_id]
         try:
